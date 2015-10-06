494a40f12b64378a9089e5df44859224852d4b45
katiebrudos <katherine.brudos@hope.edu>
2015-10-06 21:41:37 +0000

    Adding ember local-storage - not working

--- app/controllers/list.js
+++ app/controllers/list.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Controller.extend({
+
+  	list: Ember.computed.alias('model'),
+
+	actions: {
+		addItem(){
+			this.store.createRecord('list');
+			this.set('list');
+			this.save();
+		},
+		removeItem(){
+			this.store.removeRecord('list');
+		}
+	}
 });
--- app/models/list.js
+++ app/models/list.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+	
+	text: 'This is a todo msg',
+	
   
 });
